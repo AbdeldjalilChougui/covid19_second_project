279f560aa8dc85f547c722bf0e27b12378148a8c
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 08:15:17 +0100

    commit refactor

---
+++ lib/album_model.dart
@@ -0,0 +1,36 @@
+class Album {
+  final int id;
+  final int cases;
+  final int todayCases;
+  final int deaths;
+  final int todayDeaths;
+  final int recovered;
+  final int todayRecovered;
+  final int active;
+  final int population;
+
+  Album(
+      {this.id,
+        this.cases,
+        this.todayCases,
+        this.deaths,
+        this.todayDeaths,
+        this.recovered,
+        this.todayRecovered,
+        this.active,
+        this.population});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'],
+      cases: json['cases'],
+      todayCases: json['todayCases'],
+      deaths: json['deaths'],
+      todayDeaths: json['todayDeaths'],
+      recovered: json['recovered'],
+      todayRecovered: json['todayRecovered'],
+      active: json['active'],
+      population: json['population'],
+    );
+  }
+}

585e8376a7ade0c28c4364295e69ccf2f7391e69
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 08:15:20 +0100

    commit refactor

---
+++ lib/controllers/fetch_album1.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:covid19/album_model.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http.get('https://disease.sh/v3/covid-19/all');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
